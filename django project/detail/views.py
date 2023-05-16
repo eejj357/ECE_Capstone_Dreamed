00b7e51c0ae1da6c28704c6ec5f95a7727e8d981
heinheg <80655554+heinheg@users.noreply.github.com>
2023-05-16 23:04:20 +0900

    makedatabase

--- django project/detail/views.py
+++ django project/detail/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
-
+from detail.data_utils import save_auditions_from_csv
+from detail.models import Audition
+import csv
 
 def detail(request):
-    return render(request, 'detail1.html', {})
+    # 데이터가 이미 존재하는지 확인
+    if not Audition.objects.exists():
+        # 데이터가 없으면 CSV 파일에서 가져와서 저장
+        save_auditions_from_csv('C:\\Users\\heinh\\django_work\\mysite\\detail\\임시상시데이터.csv')
+
+    # 데이터 조회
+    auditions = Audition.objects.all()
+
+    return render(request, 'detail1.html', {'auditions': auditions})
