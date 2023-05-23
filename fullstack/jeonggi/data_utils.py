1ff81c3aa2407bbac99c21203a3f64982dd6d67b
heinheg <80655554+heinheg@users.noreply.github.com>
2023-05-24 03:17:41 +0900

    Add files via upload

---
+++ fullstack/jeonggi/data_utils.py
@@ -0,0 +1,44 @@
+import csv
+from jeonggi.models import Jeonggi_Audition
+
+def save_auditions_from_csv(file_path):
+    with open(file_path, 'r') as csv_file:
+        reader = csv.reader(csv_file)
+        next(reader)  # 첫 번째 행은 헤더이므로 건너뜁니다.
+        for row in reader:
+            audition_name = row[0]
+            planning_agency = row[1]
+
+            tmp1 = list(map(int, row[2].split(',')))  # ,을 기준으로 분할된 나이 리스트
+            tmp2 = list(map(int, row[3].split(',')))
+            if len(tmp1) >= 2:  # 시작 나이 조건이 2개 이상일 경우
+                age_group1 = max(tmp1)
+            else:
+                age_group1 = tmp1[0]
+            if len(tmp2) >= 2:  # 끝 나이 조건이 2개 이상일 경우
+                age_group2 = max(tmp2)
+            else:
+                age_group2 = tmp2[0]
+            recruitment_field = None if row[4] == "-" else row[4]
+            gender = None if row[5] == "-" else row[5]
+            due_date= "2099-12-30" if row[6] == "-" else row[6]
+            url = None if row[8] == "-" else row[8]
+            data_number = row[9]
+            detail_data = row[7]
+            
+            # 데이터 모델 객체 생성 및 필드 값 할당
+            jeonggi_audition_data = Jeonggi_Audition(
+                planning_agency=planning_agency,
+                audition_name=audition_name,
+                recruitment_field=recruitment_field,
+                age_group1=age_group1,
+                age_group2=age_group2,
+                gender=gender,
+                due_date=due_date,
+                url=url,
+                data_number=data_number,
+                detail_data=detail_data
+            )
+            
+            # 객체 저장
+            jeonggi_audition_data.save()
---
+++ fullstack/jeonggi/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render, get_object_or_404
+from jeonggi.data_utils import save_auditions_from_csv
+from jeonggi.models import Jeonggi_Audition
+from django.template.loader import render_to_string
+from django.db.models import Q
+import csv
+from .forms import YearForm
+
+def jeonggi_list(request):
+    # 오디션 목록 조회 로직
+
+    if not Jeonggi_Audition.objects.exists():  # 데이터가 이미 존재하는지 확인
+        # 데이터가 없으면 CSV 파일에서 가져와서 저장
+        save_auditions_from_csv('static/data/정기data.csv')
+
+    jeonggi_auditions = Jeonggi_Audition.objects.all()  # 데이터 조회
+
+    form = YearForm(initial={'year': 2000})  # 나이 form 생성
+
+    if request.method == 'POST':
+        form = YearForm(request.POST)
+        if form.is_valid():
+            year = form.cleaned_data['year']
+            # year 변수에 입력받은 연도 값이 저장됨
+            # 추가적인 로직 수행
+            print('year=', year)
+            return HttpResponseRedirect('/jeonggi')
+
+    return render(request, 'jeonggi_list.html', {'jeonggi_auditions': jeonggi_auditions, 'form': form})
+
+def jeonggi_detail(request, pk):
+    # 특정 오디션 상세 정보 조회 로직
+    audition = get_object_or_404(Jeonggi_Audition, pk=pk)
+    return render(request, 'jeonggi_detail.html', {'audition': audition})
+
+def filtered_jeonggi_list(request):
+    # POST 요청인 경우 필터링 옵션 받기
+    recruitment_field_f = request.GET.getlist('recruitment_field[]')
+    gender_f = request.GET.get('gender')
+
+    q = Q()
+    if recruitment_field_f:
+        for option in recruitment_field_f:
+            q &= Q(recruitment_field__contains=option)
+
+    if gender_f:
+        q &= Q(gender__contains=gender_f)
+
+    result = Jeonggi_Audition.objects.filter(q)
+    return render(request, 'jeonggi_list.html', {'jeonggi_auditions': result})
